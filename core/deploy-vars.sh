08f2e2c2011518de9f9eb2ea1f34a39d0258b810
mikevandersluis <mike@mybesttools.net>
2026-03-12 07:50:22 +0100

    build fix

--- core/deploy-vars.sh
+++ core/deploy-vars.sh
@@ -3,27 +3,16 @@ proj_dir=$(pwd)
 path=$(pwd)
 xtask_dir=""
 while [ "$path" != "/" ]; do
-    if [ -d "$path/esp-hal-app" ]; then
-        xtask_dir="$path/esp-hal-app"
+    if [ -d "$path/deps/esp-hal-app" ]; then
+        xtask_dir="$path/deps/esp-hal-app"
         break
     fi
     path=$(dirname "$path")
 done
 if [ -z "$xtask_dir" ]; then
-    echo "${esp-hal-app} not found" >&2
+    echo "esp-hal-app not found" >&2
     exit 1
 fi
 
-path=$(pwd)
-base_target_dir=""
-while [ "$path" != "/" ]; do
-    if [ -d "$path/hamster/${base_target}" ]; then
-        base_target_dir="$path/hamster/${base_target}"
-        break
-    fi
-    path=$(dirname "$path")
-done
-if [ -z "$base_target_dir" ]; then
-    echo "${base_target} not found" >&2
-    exit 1
-fi
+base_target_dir="$proj_dir/../build"
+
